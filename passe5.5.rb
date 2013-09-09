b10caf6bb568d455a10ecb65de9067c1a3ff7e69
gf <g@g.fr>
2013-09-09 17:01:15 +0200

    maj

--- passe5.5.rb
+++ passe5.5.rb
@@ -11,37 +11,56 @@ class Formation < ActiveRecord::Base
 end
 
 
-
+# http://www.etoile.regioncentre.fr/metier-formation/rechFormation/ficheAction.asp?hiFOR_ID=13040&modeAcces=GRP&username=guest&password=5f4dcc3b5aa765d61d8327deb882cf99&urlFrom=http://www.etoile.regioncentre.fr:80/GIP/cache/bypass/accueiletoile/seformer/formation/rechercheformation;jsessionid=05D6E4A1E0CA4D7F5977C3F99AE2686E
 
 # formations = Formation.where('url_orig like ?', 'http://www.defi-metiers.fr%')
-# formations = Formation.where('url_orig like ?', 'http://www.etoile.regioncentre.fr%')
+formations = Formation.where('url_orig like ?', 'http://www.etoile.regioncentre.fr%')
 # formations = Formation.where('url_orig like ?', 'http://www.formationauvergne.com%')
 
-formations = Formation.where('url_orig like ?', 'http://www.inffolor.org%')
+# formations = Formation.where('url_orig like ?', 'http://www.inffolor.org%')
 
 count = formations.count
 cpt = 0
 
 formations.each { |formation|
-  puts "\n================== #{cpt}/#{count} ===============\n"
   cpt += 1
+  puts "\n================== #{cpt}/#{count} ===============\n"
+  puts formation.url_orig
+  next
   outfile = "details-reprise/#{formation.dest}/#{formation.id}.html"
-  next if File.exists? outfile 
+  infile =  "details/#{formation.dest}/#{formation.id}.html"
+  # next if File.exists? outfile 
+  doc = Nokogiri::HTML(open(infile))
+  url = doc.xpath('//iframe').first[:src]
+  puts "Setting #{url}"
+  formation.update_columns(url_orig: url)
+  next
+  begin 
+    details = Nokogiri::HTML(open(url))
+    File.write(outfile, details.inner_html)
+    puts "Write #{outfile}"
+  rescue Exception => e
+    puts "Oups #{e}"
+  end
+
+  # break
+  # binding.pry
+  # doc.xpath('//iframe').first
 
   # /rechOrganisme/ficheForm.asp?hiFOR_ID=35840&rubCode=14&id=8&menuCode=14
   # query = formation.url_orig.split('url=')[1].gsub('&param=', '?')
   # url = URI.decode "http://sigma.formationauvergne.com#{query}"
   # url = "#{formation.url_orig}&resV=1366&resH=768"
-  url = formation.url_orig
-  begin
-    puts "Getting #{url}"
-    `wget '#{url}' -q -O #{outfile}`
-    # puts "wget #{url} -O #{outfile}"
-    # doc = Nokogiri::HTML(open(URI.encode(url)))
-    puts "Write to #{outfile}"
-    # File.write(outfile, doc.inner_html)
-  rescue Exception => e
-    puts "Exception: #{e}"
-  end
+  # url = formation.url_orig
+  # begin
+  #   puts "Getting #{url}"
+  #   `wget '#{url}' -q -O #{outfile}`
+  #   # puts "wget #{url} -O #{outfile}"
+  #   # doc = Nokogiri::HTML(open(URI.encode(url)))
+  #   puts "Write to #{outfile}"
+  #   # File.write(outfile, doc.inner_html)
+  # rescue Exception => e
+  #   puts "Exception: #{e}"
+  # end
 
 }
--- passe7.rb
+++ passe7.rb
@@ -5,6 +5,210 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+# 191	www.sofia.c2rp.fr
+def extract_7(formation)
+  infile = "details/#{formation.dest}/#{formation.id}.html"
+  puts "Extract_7: open #{infile}"
+  doc     = Nokogiri::HTML(open(infile))
+
+  # content = doc.xpath('//div[@id = "idDivContenu"]').first
+  # binding.pry
+
+  begin 
+    objectif = doc.xpath('//span[text() = "Objectifs :"]').first.parent.parent.next.text
+  rescue Exception => e
+    objectif = 'n/a'
+  end
+
+  begin 
+    contenu = doc.xpath('//span[text() = "Contenu de la formation  :"]').first.parent.parent.next.text
+  rescue Exception => e
+    contenu = 'n/a'
+  end
+
+  begin
+    # email    = doc.xpath('//table[@class = "ficheresultat_coordonnee"]/a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+    email    = doc.xpath('//a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+  rescue Exception => e
+    email = 'n/a'
+  end
+
+  email.strip!
+  objectif.strip!
+  contenu.strip!
+
+  puts "email:\t%#{email}%"
+  # puts "objectif:\t%#{objectif}%"
+  # puts "contenu:\t%#{contenu}%"
+
+  formation.update_columns(email: email, objectif: objectif, contenu: contenu, step: 'step7')
+  # binding.pry
+
+end
+
+# 193	www.cariforef-pdl.org
+def extract_6(formation)
+  infile = "details/#{formation.dest}/#{formation.id}.html"
+  puts "Extract_6: open #{infile}"
+  doc     = Nokogiri::HTML(open(infile))
+
+  # content = doc.xpath('//div[@id = "idDivContenu"]').first
+  # binding.pry
+
+  begin 
+    objectif = doc.xpath('//b[text() = "Objectifs :"]').first.parent.parent.next.next.text
+  rescue Exception => e
+    objectif = 'n/a'
+  end
+
+  begin 
+    contenu = doc.xpath('//b[text() = "Programme :"]').first.parent.parent.next.next.text
+  rescue Exception => e
+    contenu = 'n/a'
+  end
+
+  begin
+    # email    = doc.xpath('//table[@class = "ficheresultat_coordonnee"]/a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+    email    = doc.xpath('//a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+  rescue Exception => e
+    email = 'n/a'
+  end
+
+  email.strip!
+  objectif.strip!
+  contenu.strip!
+
+  puts "email:\t%#{email}%"
+  # puts "objectif:\t%#{objectif}%"
+  # puts "contenu:\t%#{contenu}%"
+
+  formation.update_columns(email: email, objectif: objectif, contenu: contenu, step: 'step7')
+  # binding.pry
+
+end
+
+
+# 235	www.etoile.regioncentre.fr
+def extract_5(formation)
+  infile = "details/#{formation.dest}/#{formation.id}.html"
+  puts "Extract_5: open #{infile}"
+  doc     = Nokogiri::HTML(open(infile))
+
+  content = doc.xpath('//div[@id = "idDivContenu"]').first
+  # binding.pry
+
+  begin 
+    objectif = content.xpath('//u[text() = "Objectif : "]').first.parent.parent.next.next.text
+  rescue Exception => e
+    objectif = 'n/a'
+  end
+
+  begin 
+    contenu = content.xpath('//u[text() = "Programme : "]').first.parent.parent.next.next.text
+  rescue Exception => e
+    contenu = 'n/a'
+  end
+
+  begin
+    # email    = doc.xpath('//table[@class = "ficheresultat_coordonnee"]/a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+    email    = doc.xpath('//a[starts-with(@href, "mailTo")]').first[:href].split(':')[1]
+  rescue Exception => e
+    email = 'n/a'
+  end
+
+  email.strip!
+  objectif.strip!
+  contenu.strip!
+
+  puts "email:\t%#{email}%"
+  # puts "objectif:\t%#{objectif}%"
+  # puts "contenu:\t%#{contenu}%"
+
+  formation.update_columns(email: email, objectif: objectif, contenu: contenu, step: 'step7')
+  # binding.pry
+
+end
+
+
+# 283	www.formation.gref-bretagne.com
+def extract_4(formation)
+  infile = "details/#{formation.dest}/#{formation.id}.html"
+  puts "Extract_4: open #{infile}"
+  doc     = Nokogiri::HTML(open(infile))
+
+  # binding.pry
+  objectif = ''
+
+  begin 
+    obj = doc.xpath('//strong[text() = "Objectifs "]').first
+    obj = obj.next
+    while obj.name != 'img'
+      if obj.name == 'br'
+        objectif += "\n"
+      else
+        objectif += obj.text
+      end
+      obj = obj.next
+    end
+  rescue Exception => e
+    puts "Exception #{e}"
+    objectif = 'n/a'
+  end
+  objectif.strip!
+  # begin 
+  #   contenu = doc.xpath('//td[text() = "Programme :"]').first.parent.xpath('td').last.text
+  # rescue Exception => e
+  #   contenu = 'n/a'
+  # end
+
+  begin
+    email    = doc.xpath('//a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+  rescue Exception => e
+    email = 'n/a'
+  end
+
+  puts "email:\t%#{email}%"
+  puts "objectif:\t%#{objectif}%"
+  # puts "contenu:\t%#{contenu}%"
+  contenu = ''
+  formation.update_columns(email: email, objectif: objectif, contenu: contenu, step: 'step7')
+
+
+end
+
+# 332	www.formationchampagneardenne.org
+def extract_3(formation)
+  infile = "details/#{formation.dest}/#{formation.id}.html"
+  puts "Extract_3: open #{infile}"
+  doc     = Nokogiri::HTML(open(infile))
+
+  begin 
+    objectif = doc.xpath('//td[text() = "A quoi prépare cette formation ?"]').first.parent.xpath('td').last.text
+  rescue Exception => e
+    objectif = 'n/a'
+  end
+
+  begin 
+    contenu = doc.xpath('//td[text() = "Programme :"]').first.parent.xpath('td').last.text
+  rescue Exception => e
+    contenu = 'n/a'
+  end
+
+  begin
+    email    = doc.xpath('//a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+  rescue Exception => e
+    email = 'n/a'
+  end
+
+  puts "email:\t%#{email}%"
+  puts "objectif:\t%#{objectif}%"
+  puts "contenu:\t%#{contenu}%"
+
+  formation.update_columns(email: email, objectif: objectif, contenu: contenu, step: 'step7')
+  # binding.pry
+
+end
+
 # 438	www.defi-metiers.fr
 def extract_2(formation)
 
@@ -13,7 +217,33 @@ def extract_2(formation)
   doc     = Nokogiri::HTML(open(infile))
   content = doc.xpath('//table[@class="offreDetails"]')
 
-  binding.pry
+  begin
+    objectif = content.xpath('//td[text() = "Objectif"]').first.next_sibling.text
+  rescue Exception => e
+    objectif = 'n/a'
+  end
+  # contenu  = content.xpath('//td[text() = "Description"]').first.parent.elements.last.text
+  # contenu  = content.xpath('//td[text() = "Description"]').first.parent.xpath('//td[@class = "donnees"]')elements.last.text
+  begin
+    contenu = content.xpath('//td[text() = "Description"]').first.parent.xpath('td[@class = "donnees"]').text
+  rescue Exception => e
+    contenu = 'n/a'
+  end
+
+  # binding.pry
+
+  begin
+    email    = content.xpath('//a[starts-with(@href, "mailto")]').first[:href].split(':')[1]
+  rescue Exception => e
+    email = 'n/a'
+  end
+
+  puts "email:\t%#{email}%"
+  puts "objectif:\t%#{objectif}%"
+  puts "contenu:\t%#{contenu}%"
+  
+  formation.update_columns(email: email, objectif: objectif, contenu: contenu, step: 'step7')
+
 end
 
 # 657	www.rhonealpes-orientation.org
@@ -48,11 +278,19 @@ end
 
 
 # formations = Formation.where('url_orig like ?', '%www.rhonealpes-orientation.org%')
-formations = Formation.where('url_orig like ?', '%www.defi-metiers.fr%')
+# formations = Formation.where('url_orig like ?', '%www.defi-metiers.fr%')
+# formations = Formation.where(:id =>  1608)
+# formations = Formation.where('url_orig like ?', '%www.formationchampagneardenne.org%')
+# formations = Formation.where('url_orig like ?', '%www.formation.gref-bretagne.com%')
+# formations = Formation.where('url_orig like ?', '%www.etoile.regioncentre.fr%')
+# formations = Formation.where('url_orig like ?', '%www.cariforef-pdl.org%')
+formations = Formation.where('url_orig like ?', '%www.sofia.c2rp.fr%')
+
 count = formations.count
 cpt = 0
 formations.each { |f| 
-  puts "=========== #{cpt} / #{count} ============"
+  cpt += 1
+  puts "=========== #{cpt} / #{count} / #{f.id} ============"
   if f.url_orig == 'n/a' or f.url_orig == f.url_ets or f.step == 'step7' 
     next
   end
@@ -63,16 +301,16 @@ formations.each { |f|
     extract_1 f
   when 'www.defi-metiers.fr'
     extract_2 f
-  # when 'www.formationchampagneardenne.org'
-  #   extract_3 f
-  # when 'www.formation.gref-bretagne.com'
-  #   extract_4 f
-  # when 'www.etoile.regioncentre.fr'
-  #   extract_5 f
-  # when 'www.cariforef-pdl.org'
-  #   extract_6 f
-  # when 'www.sofia.c2rp.fr'
-  #   extract_7 f
+  when 'www.formationchampagneardenne.org'
+    extract_3 f
+  when 'www.formation.gref-bretagne.com'
+    extract_4 f
+  when 'www.etoile.regioncentre.fr'
+    extract_5 f
+  when 'www.cariforef-pdl.org'
+    extract_6 f
+  when 'www.sofia.c2rp.fr'
+    extract_7 f
   # when 'offre-formation.atout-metierslr.fr'
   #   extract_8 f
   # when 'www.c2r-bourgogne.org'
@@ -97,7 +335,7 @@ formations.each { |f|
     puts "Formation #{f.id} #{f.url_orig} NON TRAITÉE"
     next
   end
-  cpt += 1
+
 }
     
 
